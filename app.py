0c9c5f1b0f541cd0a4d72a7e41fe8302d21f4f77
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-02-08 12:25:36 +0200

    fill table

--- app.py
+++ app.py
@@ -2,3 +2,21 @@ import psycopg2
 
 conn = psycopg2.connect(
     "host=127.0.0.1 dbname=test1 user=etsh password=3894")
+
+cursor = conn.cursor()
+
+# Drop table if exists to avoid error
+cursor.execute('DROP TABLE IF EXISTS table1')
+# Content tuple content for table1
+content = '''
+  id INTEGER PRIMARY KEY,
+  completed BOOLEAN NOT NULL DEFAULT False
+'''
+cursor.execute('CREATE TABLE table1 (' + content + ');')
+
+cursor.execute('INSERT INTO table1 (id, completed) VALUES (%s,%s);', (1, True))
+cursor.execute(
+    'INSERT INTO table1 (id, completed) VALUES (%s,%s);', (2, False))
+
+cursor.close()
+conn.close()
